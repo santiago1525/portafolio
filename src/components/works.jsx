04a41de3a4d4fe3f6951131f62e580d587f442c6
santiago1525 <s4nt14g03015@gmail.com>
2022-07-24 23:13:02 -0500

    add realState

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -26,10 +26,10 @@ const items = [
     github: 'https://github.com/santiago1525/relojeria'
   },
   {
-    title: 'landing page',
-    img: require('../props/landingPage.png'),
-    link: 'https://landingpage-scc.netlify.app/',
-    github: 'https://github.com/santiago1525/landingPage'
+    title: 'Real State',
+    img: require('../props/realState.png'),
+    link: 'https://real-state-wheat.vercel.app/',
+    github: 'https://github.com/santiago1525/realState'
   },
   {
     title: 'sistema usuarios',
@@ -37,6 +37,13 @@ const items = [
     link: 'http://sistema-usuarios.epizy.com',
     github: 'https://github.com/sistemausuarios'
   },
+  {
+    title: 'landing page',
+    img: require('../props/landingPage.png'),
+    link: 'https://landingpage-scc.netlify.app/',
+    github: 'https://github.com/santiago1525/landingPage'
+  },
+  
   {
     title: 'blog de cafe',
     img: require('../props/bloCafe.png'),
