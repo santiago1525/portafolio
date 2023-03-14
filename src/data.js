29aede64c9bceae68cb93b2e383fc3f756bde3b0
santiago1525 <s4nt14g03015@gmail.com>
2023-03-13 21:59:49 -0500

    add gpt3, set navbar

--- src/data.js
+++ src/data.js
@@ -1,51 +1,58 @@
 export const itemsWeb = [
-    {
-      title: 'Audiometria',
-      img: require('./props/audiometria.png'),
-      link: 'https://audiometria-scc.netlify.app/',
-      github: 'https://github.com/santiago1525/audiometria'
+  {
+    title: 'GPT 3',
+    img: require('./props/gpt3.png'),
+    link: 'https://gpt-3-santiago1525.vercel.app/',
+    github: 'https://github.com/santiago1525/GPT3'
 
-    },
-    {
-      title: 'Real State',
-      img: require('./props/realState.png'),
-      link: 'https://real-state-wheat.vercel.app/',
-      github: 'https://github.com/santiago1525/realState'
-    },
-    {
-      title: 'firebase',
-      img: require('./props/react-app.png'),
-      link: 'https://cursos-firebase.vercel.app/',
-      github: 'https://github.com/santiago1525/cursosFirebase'
-    },
-    {
-      title: 'Relojeria',
-      img: require('./props/relojeria.png'),
-      link: 'https://relojeria-scc.netlify.app/',
-      github: 'https://github.com/santiago1525/relojeria'
-    },
-    {
-      title: 'sistema usuarios',
-      img: require('./props/sistema-usuarios.png'),
-      link: 'http://sistema-usuarios.epizy.com',
-      github: 'https://github.com/sistemausuarios'
-    },
-    {
-      title: 'landing page',
-      img: require('./props/landingPage.png'),
-      link: 'https://landingpage-scc.netlify.app/',
-      github: 'https://github.com/santiago1525/landingPage'
-    },
-    
-    /* {
-      title: 'blog de cafe',
-      img: require('./props/bloCafe.png'),
-      link: 'https://blogdecafe-scc.netlify.app/',
-      github: 'https://github.com/santiago1525/blogCafe'
-    } */
-  
+  },
+  {
+    title: 'Cursos Programación',
+    img: require('./props/react-app.png'),
+    link: 'https://cursos-firebase.vercel.app/',
+    github: 'https://github.com/santiago1525/cursosFirebase'
+  },
+  {
+    title: 'Audiometria',
+    img: require('./props/audiometria.png'),
+    link: 'https://audiometria-scc.netlify.app/',
+    github: 'https://github.com/santiago1525/audiometria'
+
+  },
+  {
+    title: 'Relojeria',
+    img: require('./props/relojeria.png'),
+    link: 'https://relojeria-scc.netlify.app/',
+    github: 'https://github.com/santiago1525/relojeria'
+  },
+  {
+    title: 'Real State',
+    img: require('./props/realState.png'),
+    link: 'https://real-state-wheat.vercel.app/',
+    github: 'https://github.com/santiago1525/realState'
+  },
+  {
+    title: 'sistema usuarios',
+    img: require('./props/sistema-usuarios.png'),
+    link: 'http://sistema-usuarios.epizy.com',
+    github: 'https://github.com/sistemausuarios'
+  },
+  /* {
+    title: 'landing page',
+    img: require('./props/landingPage.png'),
+    link: 'https://landingpage-scc.netlify.app/',
+    github: 'https://github.com/santiago1525/landingPage'
+  }, */
+
+  /* {
+    title: 'blog de cafe',
+    img: require('./props/bloCafe.png'),
+    link: 'https://blogdecafe-scc.netlify.app/',
+    github: 'https://github.com/santiago1525/blogCafe'
+  } */
+
 ];
 
 export const itemsApp = [
-    
+
 ]
