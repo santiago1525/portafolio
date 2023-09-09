25b0a160ffecd219aa1b63e0f8794690bffbcb64
Your Name  git config --global user.name Your <s4nt14g03015@gmail.com>
2023-09-09 15:39:44 -0500

    update about

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,15 +4,15 @@ import Buttons from './button';
 
 function Home() {
 
-   let toogle = () => {
-        let element = document.querySelector('.hover-show');
-        if(element.classList.contains('active')) {
-          element.classList.remove('active')
-        }
-        else {
-          element.classList.add('active')
-        }
-   } 
+  let toogle = () => {
+    let element = document.querySelector('.hover-show');
+    if (element.classList.contains('active')) {
+      element.classList.remove('active')
+    }
+    else {
+      element.classList.add('active')
+    }
+  }
 
   return (
     <div id='home' className='container home-container'>
@@ -34,10 +34,19 @@ function Home() {
       <h2 id='about'>
         <span>Sobre Mi</span> <br />
         <p>
-          Diseñador web en constante aprendizaje, con experiencia en la creación de paginas web con lenguajes como HTML y CSS, he realizado proyectos de paginas web con conexión a base de datos creadas con MYSQL o Firebase, algunas administradas por medio de phpMyAdmin y desarrolladas con PHP, JAVASCRIPT y frameworks de CSS llamados Materialize o Boostrap para un diseño completamente responsivo, también he implementado proyectos trabajados con React.js a través de la creación de componentes. <br /> <br />
-          Experiencia en la creación de aplicaciones no code totalmente funcionales tanto para IOS como para ANDROID en plataformas como Glide, Adalo y FlutterFlow. <br /> <br />
-         <br />
-         Conocimiento en otros lenguajes como C++.
+          Soy un programador web en constante evolución con experiencia en la creación de páginas web utilizando lenguajes como HTML y CSS. He trabajado en proyectos con conexión a bases de datos usando MYSQL o Firebase, y he administrado algunas de ellas a través de phpMyAdmin. Mis habilidades también incluyen el desarrollo de páginas web utilizando PHP, JavaScript y frameworks de CSS como Materialize o Bootstrap para lograr un diseño completamente responsivo. Además, he implementado proyectos trabajados con React.js, empleando la creación de componentes para una experiencia de usuario dinámica y atractiva. <br /> <br />
+          <br />
+          * Conocimiento en la creación de aplicaciones nativas para Android e iOS usando Flutter.
+
+          * Experiencia en la creación de aplicaciones no code en plataformas como Glide, Adalo y FlutterFlow.
+
+          * Conocimientos intermedios en Excel.
+
+          * Conocimientos en otros lenguajes como C++ y Matlab.
+
+          * Certificado en formulación de proyectos.
+
+          * Certificado en creación de planes de marketing.
         </p>
       </h2>
 
